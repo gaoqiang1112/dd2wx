9c52a4926a6e5815ffab2add6ecc89976a8d7d2d
高强 <185559129@qq.com>
2020-06-05 12:35:44 +0800

    gq/fix  修复问题

--- methods/copyChange.js
+++ methods/copyChange.js
@@ -3,7 +3,7 @@ var path = require('path');
 var readline = require('readline');
 var os = require('os');
 
-function copyChange(filedir,projectName,NEWPROJECTENDSTR) {
+function copyChange(filedir,projectName,fileNamePre,NEWPROJECTENDSTR) {
     var reg =new RegExp(projectName);
     var newPath = filedir.split(".")[0].replace(reg,projectName+NEWPROJECTENDSTR)+'.'+filedir.split(".")[1];
     fs.copyFileSync(filedir,newPath)
